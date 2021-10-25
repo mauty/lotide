832be2d4d7f0b50798a655e0c361b8708a8e1a92
Mike Auty <m@auty.ca>
2021-10-25 19:07:46 +0000

    initial commit

---
+++ assertEqual.js
@@ -0,0 +1,12 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log("🛑 Assertion failed: " + actual + " !== " + expected);
+  } else {
+    console.log("✅ Assertion Passed: " + actual + " === " + expected);
+  }
+};
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("Bootcamp", "Bootcamp");
+assertEqual(1, 1.1);
