b35b74c9cdf90deceb83ea8561c1a09bf576d872
Mike Auty <m@auty.ca>
2021-10-26 23:24:45 +0000

    initial commit

---
+++ assertArraysEqual.js
@@ -0,0 +1,23 @@
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) {
+      return false;
+    }
+  }
+return true;
+}
+
+
+const assertArraysEqual = function(array1, array2) {
+  if (eqArrays(array1, array2) === false ) {
+    console.log(`🛑 Assertion failed: ${array1} !== ${array2}`);
+  } else {
+    console.log(`✅ Assertion Passed:  ${array1} === ${array2}`);
+  }
+};
+
+assertArraysEqual([2,3],[2,3])
+assertArraysEqual([2,3],[2,4])
