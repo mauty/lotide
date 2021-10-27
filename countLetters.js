bf6fa67cafd31f8461e45597e11760f69ffa9457
Mike Auty <m@auty.ca>
2021-10-27 22:59:14 +0000

    initial commit

---
+++ countLetters.js
@@ -0,0 +1,30 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🛑 Assertion failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅ Assertion Passed:  ${actual} === ${expected}`);
+  }
+};
+
+const countLetters = function(sentence) {
+  const result = {}
+  // let letter = "a"
+  for (let letter of sentence) {
+    // check for spaces
+    if (letter !== ' ') {
+      // console.log(`Letter: ${letter}`)
+      // add new key to result object
+    if (!result[letter]) {
+      result[letter] = 1
+    } else {
+      // increase value of key by 1
+      result[letter] += 1
+    }
+    }
+    // console.log(result)
+  }
+  return result;
+}
+
+console.log(countLetters("lets go raptors"));
+console.log(countLetters("raptors"));
