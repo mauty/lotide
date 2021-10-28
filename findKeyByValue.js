a40efe30cab8a321865b28b8019b0d51369636d0
Mike Auty <m@auty.ca>
2021-10-28 00:54:51 +0000

    initial commit

---
+++ findKeyByValue.js
@@ -0,0 +1,34 @@
+const assertEqual = function(actual, expected) {
+  if (actual !== expected) {
+    console.log(`🛑 Assertion failed: ${actual} !== ${expected}`);
+  } else {
+    console.log(`✅ Assertion Passed:  ${actual} === ${expected}`);
+  }
+};
+
+const findKeyByValue = function(bestTVShowsByGenre, value) {
+  // iterate through object to find value
+  let listOfKeys = [];
+  listOfKeys = Object.keys(bestTVShowsByGenre);
+  // console.log(`List of keys: ${listOfKeys}`);
+  for (let item of listOfKeys) {
+    if (bestTVShowsByGenre[item] === value) {
+      return item;
+    }
+  }
+  // return the key associated with that value
+  // if no value is found return undefined
+};
+
+
+
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
